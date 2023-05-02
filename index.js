a08c99e49cd48f2f3f1b34568672e7c5959c199f
aniketgarole <aniketgarole11@gmail.com>
2023-05-02 16:42:14 +0530

    added cors

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const {postsRouter} = require("./routes/posts.routes")
 const { auth } = require("./middlewares/auth.middleware")
 require("dotenv").config()
 
+const cors = require("cors")
+
+
+
 
 const app = express()
 app.use(express.json())
 
-
+app.use(cors())
 app.use("/users", userRouter)
 
 
